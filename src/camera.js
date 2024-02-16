1e9bfd8c117733a906d31f368a31d8d55373e897
naresh <nareshm1997kumar@gmail.com>
2024-02-16 22:33:12 +0530

    chore(): adds camera movement

---
+++ src/camera.js
@@ -0,0 +1,61 @@
+import * as THREE from "three";
+
+export function createCamera(gameWindow) {
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    gameWindow.offsetWidth / gameWindow.offsetHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+
+  let cameraRadius = 4;
+  let cameraAzimuth = 0;
+  let cameraElevation = 0;
+  let isMouseDown = false;
+  let prevMouseX = 0;
+  let prevMouseY = 0;
+  updateCameraPosition();
+
+  function onMouseDown() {
+    isMouseDown = true;
+  }
+  function onMouseUp() {
+    isMouseDown = false;
+  }
+  function onMouseMove(event) {
+    if (isMouseDown) {
+      cameraAzimuth += -((event.clientX - prevMouseX) * 0.5);
+      cameraElevation += (event.clientY - prevMouseY) * 0.5;
+      cameraElevation = Math.min(90, Math.max(0, cameraElevation));
+      updateCameraPosition();
+    }
+
+    prevMouseX = event.clientX;
+    prevMouseY = event.clientY;
+  }
+
+  function updateCameraPosition() {
+    camera.position.x =
+      cameraRadius *
+      Math.sin((cameraAzimuth * Math.PI) / 180) *
+      Math.cos((cameraElevation * Math.PI) / 180);
+
+    camera.position.y =
+      cameraRadius * Math.sin((cameraElevation * Math.PI) / 180);
+
+    camera.position.z =
+      cameraRadius *
+      Math.cos((cameraAzimuth * Math.PI) / 180) *
+      Math.cos((cameraElevation * Math.PI) / 180);
+
+    camera.lookAt(0, 0, 0);
+    camera.updateMatrix();
+  }
+  return {
+    camera,
+    onMouseDown,
+    onMouseUp,
+    onMouseMove,
+  };
+}
--- src/scene.js
+++ src/scene.js
@@ -1,17 +1,12 @@
 import * as THREE from "three";
+import { createCamera } from "./camera";
 
 export function createScene() {
   const gameWindow = document.getElementById("render-target");
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0x777777);
 
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    gameWindow.offsetWidth / gameWindow.offsetHeight,
-    0.1,
-    1000
-  );
-  camera.position.z = 5;
+  const camera = createCamera(gameWindow);
 
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
@@ -24,9 +19,9 @@ export function createScene() {
   scene.add(mesh);
 
   function draw() {
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-    renderer.render(scene, camera);
+    // mesh.rotation.x += 0.01;
+    // mesh.rotation.y += 0.01;
+    renderer.render(scene, camera.camera);
   }
 
   function start() {
@@ -37,8 +32,21 @@ export function createScene() {
     renderer.setAnimationLoop(null);
   }
 
+  function onMouseDown() {
+    camera.onMouseDown();
+  }
+  function onMouseUp() {
+    camera.onMouseUp();
+  }
+  function onMouseMove(event) {
+    camera.onMouseMove(event);
+  }
+
   return {
     start,
     stop,
+    onMouseDown,
+    onMouseUp,
+    onMouseMove,
   };
 }
