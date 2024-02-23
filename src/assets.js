266dbcba2ca603b03911a562812bbff484494f04
Naresh Mayakrishnan <nareshm1997kumar@gmail.com>
2024-02-23 23:39:58 +0800

    chore(): add asset

---
+++ src/assets.js
@@ -0,0 +1,44 @@
+import * as THREE from "three";
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const assets = {
+    'grass': (x, y) => {
+        const material = new THREE.MeshLambertMaterial({ color: 0x00aa00 });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.userData = { id: 'grass' };
+        mesh.position.set(x, -0.5, y);
+        return mesh;
+    },
+    'building-1': (x, y) => {
+        const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.userData = { id: 'building-1' };
+        mesh.position.set(x, 0.5, y);
+        return mesh;
+    },
+    'building-2': (x, y) => {
+        const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.userData = { id: 'building-2' };
+        mesh.scale.set(1, 2, 1);
+        mesh.position.set(x, 1, y);
+        return mesh;
+    },
+    'building-3': (x, y) => {
+        const material = new THREE.MeshLambertMaterial({ color: 0x777777 });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.userData = { id: 'building-3' };
+        mesh.scale.set(1, 3, 1);
+        mesh.position.set(x, 1.5, y);
+        return mesh;
+    },
+}
+
+export function createAssets(assetId, x, y) {
+    if (assetId in assets) {
+        return assets[assetId](x, y);
+    } else {
+        console.warn('No Asset found');
+        return undefined;
+    }
+}
--- src/scene.js
+++ src/scene.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { createCamera } from "./camera";
+import { createAssets } from "./assets";
 
 export function createScene() {
   const gameWindow = document.getElementById("render-target");
@@ -13,25 +14,22 @@ export function createScene() {
   gameWindow.appendChild(renderer.domElement);
 
   let terrain = [];
-  let building = [];
+  let buildings = [];
 
   function initialize(city) {
     scene.clear();
     terrain = [];
-    building = [];
+    buildings = [];
     for (let x = 0; x < city.size; x++) {
       const column = [];
       for (let y = 0; y < city.size; y++) {
-        // Grass
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshLambertMaterial({ color: 0x00aa00 });
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(x, -0.5, y);
+        const terrainId = city.data[x][y].terrainId;
+        const mesh = createAssets(terrainId, x, y);
         scene.add(mesh);
         column.push(mesh);
       }
       terrain.push(column);
-      building.push([...Array(city.size)]);
+      buildings.push([...Array(city.size)]);
     }
 
     setupLights();
@@ -40,24 +38,19 @@ export function createScene() {
   function update(city) {
     for (let x = 0; x < city.size; x++) {
       for (let y = 0; y < city.size; y++) {
-        const tile = city.data[x][y];
+        const currentBuildingId = buildings[x][y]?.userData.id;
+        const newBuildingId = city.data[x][y].buildingId;
 
-        if (tile.building && tile.building.startsWith('building')) {
-          // Building
-          const height = Number(tile.building.slice(-1));
-          const buildingGeometry = new THREE.BoxGeometry(1, height, 1);
-          const buildingMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
-          const buildingMesh = new THREE.Mesh(buildingGeometry, buildingMaterial);
-          buildingMesh.position.set(x, height / 2, y);
-
-          if(building[x][y]){
-            scene.remove(building[x][y]);
-          }
-
-          scene.add(buildingMesh);
-          building[x][y] = buildingMesh;
+        if (!newBuildingId && currentBuildingId) {
+          scene.remove(buildings[x][y]);
+          buildings[x][y] = undefined;
         }
 
+        if (newBuildingId !== currentBuildingId) {
+          scene.remove(buildings[x][y]);
+          buildings[x][y] = createAssets(newBuildingId, x, y);
+          scene.add(buildings[x][y]);
+        }
       }
     }
   }
